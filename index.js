f3cc7402963be97bc26edcf0b0b02e408ceba2a1
Jack <jackdelivery121@gmail.com>
2024-04-02 16:42:41 +0500

    update api

--- index.js
+++ index.js
@@ -1,120 +1,147 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const path = require('path');
-const cors = require('cors'); 
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
 const app = express();
-const multer = require('multer');
+const multer = require("multer");
 const PORT = process.env.PORT || 5000; // You can change this to any port you prefer
 app.use(cors());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // MongoDB Connection
 // mongodb+srv://sheikhfaizaan608:<password>@cluster0.wmjgu40.mongodb.net/
 
-mongoose.connect(process.env.mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('MongoDB Connected');
-}).catch(err => {
-  console.log('MongoDB Connection Error: ', err);
-});
+mongoose
+  .connect(process.env.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection Error: ", err);
+  });
 
 // Models
-const User = require('./models/User');
-const Books = require('./models/Books');
-const Requests = require('./models/Requests');
+const User = require("./models/User");
+const Books = require("./models/Books");
+const Requests = require("./models/Requests");
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './uploads'); // Uploads will be stored in the "uploads/" directory
+    cb(null, "./uploads"); // Uploads will be stored in the "uploads/" directory
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname); // File name will be timestamp + original name
-  }
+    cb(null, Date.now() + "-" + file.originalname); // File name will be timestamp + original name
+  },
 });
 
 const upload = multer({ storage: storage });
 // Routes
-app.post('/signup', async (req, res) => {
+app.post("/signup", async (req, res) => {
   try {
-    const { username, email, password,cpassword } = req.body;
+    const { username, email, password, cpassword } = req.body;
+    // user already exists
+    const isUser = await User.findOne({ email });
+    if (isUser) {
+      return res.status(404).json({ error: "user already exists" });
+    }
     // Create a new user
-    const user = new User({ username, email, password,cpassword });
+    const user = new User({ username, email, password, cpassword });
     await user.save();
-    res.status(201).json({ message: 'User created successfully', user });
+    res.status(201).json({ message: "User created successfully", user });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
 });
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   try {
     const { username, password } = req.body;
-    
+
     // Find user by username
     const user = await User.findOne({ username });
 
     if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+      return res.status(404).json({ error: "User not found" });
     }
 
     // Check if password matches
     if (user.password !== password) {
-      return res.status(401).json({ error: 'Invalid password' });
+      return res.status(401).json({ error: "Invalid password" });
     }
 
     // Password is correct
-    res.status(200).json({ message: 'Login successful', user });
+    res.status(200).json({ message: "Login successful", user });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
 });
 
-
-app.get('/users',async (req,res) => {
-    try {
-        const users = await User.find({},'username email password')
-        res.status(200).json(users)
-    }catch{
-        res.status(500).json({error:'Internal server error'})
-    }
-})
-app.delete('/users/:id', async (req, res) => {
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({}, "username email password");
+    res.status(200).json(users);
+  } catch {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+app.delete("/users/:id", async (req, res) => {
   const { id } = req.params;
 
   try {
     const deletedUser = await User.findByIdAndDelete(id);
-    
+
     if (!deletedUser) {
-      return res.status(404).json({ error: 'User not found' });
+      return res.status(404).json({ error: "User not found" });
     }
 
-    res.status(200).json({ message: 'User deleted successfully', deletedUser });
+    res.status(200).json({ message: "User deleted successfully", deletedUser });
   } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 ///books///
-app.post('/addbooks', upload.single('images'),async (req, res) => {
+app.post("/addbooks", upload.single("images"), async (req, res) => {
   try {
-    const { carname, seater, mileage,company,color,model,borowedby,status,price } = req.body;
+    const {
+      carname,
+      seater,
+      mileage,
+      company,
+      color,
+      model,
+      borowedby,
+      status,
+      price,
+    } = req.body;
     console.log(req.body);
     console.log(req.file);
     // const file = req.file.path;
 
     // Create a new user
-    const book = new Books({ carname, seater, mileage,company,color,model,borowedby,status,price,images:req.file.path});
+    const book = new Books({
+      carname,
+      seater,
+      mileage,
+      company,
+      color,
+      model,
+      borowedby,
+      status,
+      price,
+      images: req.file.path,
+    });
     await book.save();
-    res.status(201).json({ message: 'Car added successfully', book });
+    res.status(201).json({ message: "Car added successfully", book });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
-  
 });
-app.put('/books/:id', upload.single('images'), async (req, res) => {
+app.put("/books/:id", upload.single("images"), async (req, res) => {
   const bookId = req.params.id;
   const updateFields = req.body;
 
@@ -131,84 +158,89 @@ app.put('/books/:id', upload.single('images'), async (req, res) => {
     });
 
     if (!updatedBook) {
-      return res.status(404).json({ error: 'Car not found' });
+      return res.status(404).json({ error: "Car not found" });
     }
 
-    res.status(201).json({ message: 'Car updated successfully', updatedBook });
+    res.status(201).json({ message: "Car updated successfully", updatedBook });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ error: "Internal server error" });
   }
 });
-app.get('/books',async (req,res) => {
+app.get("/books", async (req, res) => {
   try {
-      const users = await Books.find({},'carname seater mileage company color model borowedby price status images')
-      res.status(200).json(users)
-  }catch{
-      res.status(500).json({error:'Internal server error'})
+    const users = await Books.find(
+      {},
+      "carname seater mileage company color model borowedby price status images"
+    );
+    res.status(200).json(users);
+  } catch {
+    res.status(500).json({ error: "Internal server error" });
   }
-})
-app.get('/books/:id', async (req, res) => {
+});
+app.get("/books/:id", async (req, res) => {
   try {
-    const bookId = req.params.id; 
-    const book = await Books.findById(bookId); 
+    const bookId = req.params.id;
+    const book = await Books.findById(bookId);
 
     if (!book) {
-     
-      return res.status(404).json({ error: 'Car not found' });
+      return res.status(404).json({ error: "Car not found" });
     }
 
-    
     res.status(200).json(book);
   } catch (error) {
-
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
-
 // Delete Book API (DELETE)
-app.delete('/books/:id', async (req, res) => {
+app.delete("/books/:id", async (req, res) => {
   const bookId = req.params.id;
 
   try {
     const deletedBook = await Books.findByIdAndDelete(bookId);
 
     if (!deletedBook) {
-      return res.status(404).json({ error: 'Car not found' });
+      return res.status(404).json({ error: "Car not found" });
     }
 
-    res.status(200).json({ message: 'Car deleted successfully' });
+    res.status(200).json({ message: "Car deleted successfully" });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
 ///requests///
 // {fullname,address,phone,email,accountno}
-app.post('/userequest',async (req, res) => {
+app.post("/userequest", async (req, res) => {
   try {
-    const {fullname,address,phone,email,accountno,status,reqfor} = req.body;
+    const { fullname, address, phone, email, accountno, status, reqfor } =
+      req.body;
     console.log(req.body);
-   
 
     // Create a new user
-    const request = new Requests({fullname,address,phone,email,accountno,status,reqfor});
+    const request = new Requests({
+      fullname,
+      address,
+      phone,
+      email,
+      accountno,
+      status,
+      reqfor,
+    });
     await request.save();
-    res.status(201).json({ message: 'Request send successfully', request });
+    res.status(201).json({ message: "Request send successfully", request });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
-  
 });
-app.put('/userequest/:id', async (req, res) => {
+app.put("/userequest/:id", async (req, res) => {
   const bookId = req.params.id;
   const updateFields = req.body;
 
   try {
     let updatedBook;
-   
 
     // Find and update the book with the provided ID
     updatedBook = await Requests.findByIdAndUpdate(bookId, updateFields, {
@@ -216,37 +248,42 @@ app.put('/userequest/:id', async (req, res) => {
     });
 
     if (!updatedBook) {
-      return res.status(404).json({ error: 'Request not found' });
+      return res.status(404).json({ error: "Request not found" });
     }
 
-    res.status(201).json({ message: 'Request updated successfully', updatedBook });
+    res
+      .status(201)
+      .json({ message: "Request updated successfully", updatedBook });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ error: "Internal server error" });
   }
 });
-app.get('/userequest',async (req,res) => {
+app.get("/userequest", async (req, res) => {
   try {
-      const Request = await Requests.find({},'fullname address phone email accountno status reqfor')
-      res.status(200).json(Request)
-  }catch{
-      res.status(500).json({error:'Internal server error'})
+    const Request = await Requests.find(
+      {},
+      "fullname address phone email accountno status reqfor"
+    );
+    res.status(200).json(Request);
+  } catch {
+    res.status(500).json({ error: "Internal server error" });
   }
-})
-app.delete('/userequest/:id', async (req, res) => {
+});
+app.delete("/userequest/:id", async (req, res) => {
   const reqId = req.params.id;
 
   try {
     const deletedReq = await Requests.findByIdAndDelete(reqId);
 
     if (!deletedReq) {
-      return res.status(404).json({ error: 'Request not found' });
+      return res.status(404).json({ error: "Request not found" });
     }
 
-    res.status(200).json({ message: 'Request deleted successfully' });
+    res.status(200).json({ message: "Request deleted successfully" });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 // Start the server
